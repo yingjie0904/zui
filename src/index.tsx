526462cf9ddbbf618abe97a7dedd318ee6dfd6cc
zhaoyingjie <zhaoyingjie@neoteched.com>
2020-08-25 18:41:20 +0800

    "build-storybook": "build-storybook --no-dll --quiet"

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-export * from "./components/button";
+export { default as Button } from "./components/button";
 export * from "./components/shared/global";
 export * from "./components/shared/styles";
 export * from "./components/shared/animation";
