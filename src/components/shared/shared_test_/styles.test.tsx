8996d95495d17ae62b16f6c41e725a6f4c4923bf
zhaoyingjie <zhaoyingjie@neoteched.com>
2020-08-30 17:48:40 +0800

    remove button.test.tsx.snap

--- src/components/shared/shared_test_/styles.test.tsx
+++ src/components/shared/shared_test_/styles.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { GlobalStyle } from "../shared/global";
+import { GlobalStyle } from "../global";
 
 describe("test global style", () => {
     it("should render the correct global style", () => {
