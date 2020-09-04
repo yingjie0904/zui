254d9f35b29d028c25ceab62d642d249240e57a3
zhaoyingjie <zhaoyingjie@neoteched.com>
2020-09-04 12:03:39 +0800

    add badge radio, microbundle

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
 export { default as Button } from "./components/button";
+export { Icon } from './components/icon'
+export { Badge } from './components/badge'
+
 export * from "./components/shared/global";
 export * from "./components/shared/styles";
 export * from "./components/shared/animation";
