6a45ef09cafc936dc7c9af7ddc57a1ae4192526a
zhaoyingjie <zhaoyingjie@neoteched.com>
2020-09-04 15:39:08 +0800

    add carousel comp

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 export { default as Button } from "./components/button";
 export { Icon } from './components/icon'
 export { Badge } from './components/badge'
+export { Carousel } from './components/carousel'
 
 export * from "./components/shared/global";
 export * from "./components/shared/styles";
