269122adbd24ab6c24c470fe413c81f234a5f5a7
zhaoyingjie <zhaoyingjie@neoteched.com>
2020-09-05 17:41:53 +0800

    add upload

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ export { default as Button } from "./components/button";
 export { Icon } from './components/icon'
 export { Badge } from './components/badge'
 export { Carousel } from './components/carousel'
+export { Avatar } from './components/avatar'
+export { Progress } from './components/progress'
 
 export * from "./components/shared/global";
 export * from "./components/shared/styles";
