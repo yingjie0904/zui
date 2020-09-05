5b0765dfb1d34a48c6f7173860d1b372e690f9e7
zhaoyingjie <zhaoyingjie@neoteched.com>
2020-09-05 10:51:46 +0800

    add message modal

---
+++ src/components/modal/__test__/modal.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import { Modal } from "../index";
+import { color, typography } from "../../shared/styles";
+
+describe("test Modal component", () => {
+    it(" ", () => {})
+})
--- src/components/shared/animation.tsx
+++ src/components/shared/animation.tsx
@@ -20,3 +20,12 @@ export const progressFlash = keyframes`
     width: 100%; 
   }
 `;
+
+export const iconSpin = keyframes`
+  0% {
+     transform: rotate(0deg);
+  }
+  100% {
+     transform: rotate(360deg);
+  }
+`;
