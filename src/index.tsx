9a3777b4e33ba7f19188df416a1be25e1427a26f
zhaoyingjie <zhaoyingjie@neoteched.com>
2020-09-07 12:20:04 +0800

    modify index.tsx(export *)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
-export { default as Button } from "./components/button";
-export { Icon } from './components/icon'
-export { Badge } from './components/badge'
-export { Carousel } from './components/carousel'
-export { Avatar } from './components/avatar'
-export { Progress } from './components/progress'
-
 export * from "./components/shared/global";
 export * from "./components/shared/styles";
 export * from "./components/shared/animation";
+export * from "./components/button";
+export * from "./components/button";
+export * from "./components/avatar";
+export * from "./components/badge";
+export * from "./components/carousel";
+export * from "./components/highlight";
+export * from "./components/icon";
+export * from "./components/message";
+export * from "./components/modal";
+export * from "./components/progress";
+export * from "./components/radio";
+export * from "./components/upload";
+export * from "./components/tree";
+export * from "./components/datepicker";
+export * from "./components/pagination";
+export * from "./components/table";
