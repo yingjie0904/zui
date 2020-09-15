0b8a6ad1df50c88767b3a1caddca5cbb1c42477f
zhaoyingjie <zhaoyingjie@neoteched.com>
2020-09-15 12:34:29 +0800

    modify index.tsx-2

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
+export { Button } from "./components/button";
+export { Icon } from './components/icon'
+export { Badge } from './components/badge'
+export { Carousel } from './components/carousel'
+export { Avatar } from './components/avatar'
+export { Progress } from './components/progress'
+export { Highlight } from './components/highlight'
+export { message, Message } from './components/message'
+export { Modal } from './components/modal'
+export { Radio } from './components/radio'
+export { Upload } from './components/upload'
+export { Tree } from './components/tree'
+export { DatePicker } from './components/datepicker'
+export { Pagination } from './components/pagination'
+export { Table } from './components/table'
 export * from "./components/shared/global";
 export * from "./components/shared/styles";
 export * from "./components/shared/animation";
-export * from "./components/button";
-export * from "./components/avatar";
-export * from "./components/badge";
-export * from "./components/carousel";
-export * from "./components/highlight";
-export * from "./components/icon";
-export * from "./components/message";
-export * from "./components/modal";
-export * from "./components/progress";
-export * from "./components/radio";
-export * from "./components/upload";
-export * from "./components/tree";
-export * from "./components/datepicker";
-export * from "./components/pagination";
-export * from "./components/table";
