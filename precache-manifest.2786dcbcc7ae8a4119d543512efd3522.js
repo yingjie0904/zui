2e3b1f0827e7ac21f940d90952b7a097a4b7e1bc
yingjie0904 <729714579@qq.com>
2020-09-15 04:37:47 +0000

    Deploying to gh-pages from  @ 0b8a6ad1df50c88767b3a1caddca5cbb1c42477f 🚀

---
+++ precache-manifest.2786dcbcc7ae8a4119d543512efd3522.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e861f28c44e3ee8a5c898be279b470",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.3fb78f8e91cd23e6b723.bundle.js"
+  },
+  {
+    "url": "runtime~main.3fb78f8e91cd23e6b723.bundle.js"
+  },
+  {
+    "url": "vendors~main.3fb78f8e91cd23e6b723.bundle.js"
+  },
+  {
+    "url": "vendors~main.3fb78f8e91cd23e6b723.bundle.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.57ce26d84f3cf8c023ebe9a3569069f2.js"
+  "precache-manifest.2786dcbcc7ae8a4119d543512efd3522.js"
 );
 
 self.addEventListener('message', (event) => {
